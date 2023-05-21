70cafc7ac8e9db4b27a0aa449836765aaf384809
santhanamsandel <107601976+santhanamsandel@users.noreply.github.com>
2023-05-21 20:28:55 +0530

    Delete src directory

--- src/app/User1.ts
+++
@@ -1,9 +0,0 @@
-export class User1 {
-    
-      public userId?:number;
-      public name?: string;
-      public mobile?: string;
-      public password?: string;
-      public role?: string;
-
-  }
