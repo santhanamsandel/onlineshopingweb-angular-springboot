73e23877d95d285d0815858baa86b13ed7dd5f40
santhanamsandel <107601976+santhanamsandel@users.noreply.github.com>
2023-05-23 18:44:06 +0530

    Delete src directory

--- src/app/UserUpdate.ts
+++
@@ -1,9 +0,0 @@
-export class UserUpdate {
-  constructor(
-    public UserId?:number,
-    public name: string = '',
-    public mobile: string = '',
-    public password: string = '',
-    public role: string = ''
-  ) {}
-}
